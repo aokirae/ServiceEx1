6939f2eb1e2ae99c8240b3f9bd621d3d187fbb7a
NaCl <kamima.yuuya@gmail.com>
2018-06-01 18:57:44 +0900

    toastされない……

--- app/src/main/java/jp/ac/titech/itpro/sdl/serviceex1/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/serviceex1/MainActivity.java
@@ -1,13 +1,21 @@
 package jp.ac.titech.itpro.sdl.serviceex1;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private final static String TAG = "MainActivity";
+
+    private BroadcastReceiver btReceiver;
+    private IntentFilter btFilter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +23,44 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "onCreate in " + Thread.currentThread());
         setContentView(R.layout.activity_main);
 
+        btReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                String action = intent.getAction();
+                Log.d(TAG, "onReceive: " + action);
+                if (action == null) return;
+                switch (action){
+                    case TestService3.ACTION_ANSWER:
+                        Log.d(TAG, "btReceiver in " + Thread.currentThread());
+                        String keyword = intent.getStringExtra(TestService3.EXTRA_ANSWER);
+                        Log.d(TAG, "keyword : " + keyword);
+                        Toast.makeText(context, "answer :" + keyword, Toast.LENGTH_SHORT).show();
+                        break;
+                    default:
+                        Log.d(TAG, "default");
+                        break;
+                }
+            }
+        };
+
+        btFilter = new IntentFilter();
+        btFilter.addAction(TestService3.ACTION_ANSWER);
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+        registerReceiver(btReceiver, btFilter);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+        unregisterReceiver(btReceiver);
+    }
+
 
     public void pressTest1(View v) {
         testService1();
@@ -24,6 +69,8 @@ public class MainActivity extends AppCompatActivity {
     public void pressTest2(View v) {
         testService2();
     }
+
+    public void pressTest3(View v) { testServise3(); }
 
     private void testService1() {
         Log.d(TAG, "testService1 in " + Thread.currentThread());
@@ -38,5 +85,13 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra(TestService2.EXTRA_MYARG, "Hello, Service2");
         startService(intent);
     }
+
+    private void testServise3(){
+        Log.d(TAG, "testService3 in" + Thread.currentThread());
+        Intent intent = new Intent(this, TestService3.class);
+        intent.putExtra(TestService3.EXTRA_MYARG, "Hello, Service3");
+        startService(intent);
+    }
+
 
 }
